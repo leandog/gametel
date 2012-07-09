8775585a645df543e330f503893dc8bc53fdb4d5
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-07-09 17:20:04 -0400

    started creation of platform

--- lib/gametel.rb
+++ lib/gametel.rb
@@ -1,17 +1,23 @@
-require 'calabash-android/cucumber'
 require 'gametel/accessors'
 require 'gametel/navigation'
 require 'gametel/version'
+require 'gametel/platforms/calabash'
 
 module Gametel
   include Calabash::Android::Operations
+
+  attr_reader :platform
 
   def self.included(cls)
     cls.extend Gametel::Accessors
   end
 
+  def initialize(pform = :calabash)
+    @platform = Gametel::Platforms::Calabash if pform == :calabash
+  end
+
   def wait_for_text(text_to_find)
-    performAction 'wait_for_text', text_to_find
+    platform.wait_for_text(text_to_find)
   end
 end
 
---
+++ lib/gametel/platforms/calabash.rb
@@ -0,0 +1,11 @@
+require 'calabash-android/cucumber'
+
+module Gametel
+  module Platforms
+    module Calabash
+      def wait_for_text(text_to_find)
+        performAction 'wait_for_text', text_to_find
+      end
+    end
+  end
+end
