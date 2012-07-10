8ec7303b0260b5b28b613d8f4087d7335f85fb67
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-07-10 09:07:53 -0400

    added two more specs

--- spec/lib/gametel_spec.rb
+++ spec/lib/gametel_spec.rb
@@ -5,6 +5,18 @@ class SampleScreen
 end
 
 describe Gametel do
+
+  it "should initialize the platform to Calabash when no other platform is specified" do
+    screen = SampleScreen.new
+    platform = screen.instance_variable_get(:@platform)
+    platform.should be_instance_of ::Gametel::Platforms::Calabash
+  end
+
+  it "should add the accessors to the class when included" do
+    SampleScreen.should respond_to :text
+    SampleScreen.should respond_to :button
+  end
+  
   context "when using Calabash" do
     let(:screen) { SampleScreen.new }
     let(:platform) { calabash_platform(screen) }
