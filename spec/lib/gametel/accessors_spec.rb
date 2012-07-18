e745a483719a0dcbb2314d754e056788e64b7085
Levi Wilson <levi@leviwilson.com>
2012-07-18 03:01:34 -0400

    restructured accessor tests

--- spec/lib/gametel/accessors_spec.rb
+++ spec/lib/gametel/accessors_spec.rb
@@ -22,81 +22,93 @@ describe Gametel::Accessors do
   context "when using Calabash" do
     let(:screen) { AccessorsSampleScreen.new }
     let(:platform) { calabash_platform(screen) }
-    
-    it "should know how to press a list item by text" do
-      platform.should_receive(:performAction).with('click_on_text', 'first item')
-      screen.first_list_item_text
+
+    context "list items" do
+      it "should know how to be chosen by text" do
+        platform.should_receive(:performAction).with('click_on_text', 'first item')
+        screen.first_list_item_text
+      end
+
+      it "should know how to be chosen by index" do
+        platform.should_receive(:performAction).with('press_list_item', 1, 0)
+        screen.first_list_item_index
+      end
     end
 
-    it "should know how to press a list item by index" do
-      platform.should_receive(:performAction).with('press_list_item', 1, 0)
-      screen.first_list_item_index
+    context "text controls" do
+      it "should know how to enter text using an index" do
+        platform.should_receive(:performAction).with('enter_text_into_numbered_field', 'blah', 3)
+        screen.first_name_index = 'blah'
+      end
+
+      it "should know how to enter text using the name" do
+        platform.should_receive(:performAction).with('enter_text_into_named_field', 'blah', 'Some name')
+        screen.first_name_name = 'blah'
+      end
+
+      it "should know how to be cleared using an index" do
+        platform.should_receive(:performAction).with('clear_numbered_field', 3)
+        screen.clear_first_name_index
+      end
+
+      it "should know how to be cleared by name" do
+        platform.should_receive(:performAction).with('clear_named_field', 'Some name')
+        screen.clear_first_name_name
+      end
     end
 
-    it "should know how to enter text into a text field using index" do
-      platform.should_receive(:performAction).with('enter_text_into_numbered_field', 'blah', 3)
-      screen.first_name_index = 'blah'
-    end
+    context "buttons" do
+      it "should know how to be chosen by text" do
+        platform.should_receive(:performAction).with('press_button_with_text', 'Save')
+        screen.save_text
+      end
 
-    it "should know how to enter text into a text field using the name" do
-      platform.should_receive(:performAction).with('enter_text_into_named_field', 'blah', 'Some name')
-      screen.first_name_name = 'blah'
-    end
+      it "should know how to be chosen by index" do
+        platform.should_receive(:performAction).with('press_button_number', 2)
+        screen.save_index
+      end
 
-    it "should know how to clear a text using index" do
-      platform.should_receive(:performAction).with('clear_numbered_field', 3)
-      screen.clear_first_name_index
-    end
-
-    it "should know how to clear a text using name" do
-      platform.should_receive(:performAction).with('clear_named_field', 'Some name')
-      screen.clear_first_name_name
-    end
-
-    it "should know how to press a button by text" do
-      platform.should_receive(:performAction).with('press_button_with_text', 'Save')
-      screen.save_text
-    end
-
-    it "should know how to press a button by index" do
-      platform.should_receive(:performAction).with('press_button_number', 2)
-      screen.save_index
-    end
-
-    it "should know how to press a button by id" do
-      platform.should_receive(:performAction).with('click_on_view_by_id', 'some_button_id')
-      screen.save_id
+      it "should know how to be chosen by id" do
+        platform.should_receive(:performAction).with('click_on_view_by_id', 'some_button_id')
+        screen.save_id
+      end
     end
 
 
-    it "should know how to check a checkbox by index" do
-      platform.should_receive(:performAction).with('toggle_numbered_checkbox', 1)
-      screen.checkbox_index
+    context "checkboxes" do
+      it "should know how to be checked by index" do
+        platform.should_receive(:performAction).with('toggle_numbered_checkbox', 1)
+        screen.checkbox_index
+      end
+
+      it "should know how to be checked by text" do
+        platform.should_receive(:performAction).with('click_on_text', 'Checkbox 2')
+        screen.checkbox_text
+      end
+
+      it "should know how to be checked by id" do
+        platform.should_receive(:performAction).with('click_on_view_by_id', 'some_check_id')
+        screen.checkbox_id
+      end
     end
 
-    it "should know how to check a checkbox by text" do
-      platform.should_receive(:performAction).with('click_on_text', 'Checkbox 2')
-      screen.checkbox_text
+    context "radio buttons" do
+      it "should know how to be clicked by text" do
+        platform.should_receive(:performAction).with('click_on_text', 'Radio Button 1')
+        screen.radio_text
+      end
+
+      it "should know how to be clicked by id" do
+        platform.should_receive(:performAction).with('click_on_view_by_id', 'some_radio_id')
+        screen.radio_id
+      end
     end
 
-    it "should know how to check a checkbox by id" do
-      platform.should_receive(:performAction).with('click_on_view_by_id', 'some_check_id')
-      screen.checkbox_id
-    end
-
-    it "should know how to click a radio button by text" do
-      platform.should_receive(:performAction).with('click_on_text', 'Radio Button 1')
-      screen.radio_text
-    end
-
-    it "should know how to click a radio button by id" do
-      platform.should_receive(:performAction).with('click_on_view_by_id', 'some_radio_id')
-      screen.radio_id
-    end
-
-    it "should know how to click a view by its id" do
-      platform.should_receive(:performAction).with('click_on_view_by_id', 'some_view_id')
-      screen.view_id
+    context "views" do
+      it "should know how to be clicked by id" do
+        platform.should_receive(:performAction).with('click_on_view_by_id', 'some_view_id')
+        screen.view_id
+      end
     end
   end
 end
