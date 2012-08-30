e0550fa6a1e2ac15578c19f7965a521b62e0ae72
Levi Wilson <levi@leviwilson.com>
2012-08-29 22:29:20 -0400

    only uninstall / install once

--- features/support/app_installation_hooks.rb
+++ features/support/app_installation_hooks.rb
@@ -2,18 +2,18 @@ require 'calabash-android/management/app_installation'
 
 AfterConfiguration do |config|
   FeatureNameMemory.feature_name = nil
+  uninstall_apps
+  install_app(ENV["TEST_APP_PATH"])
+  install_app(ENV["APP_PATH"])
 end
 
 Before do |scenario|
   feature_name = scenario.feature.name
   if FeatureNameMemory.feature_name != feature_name
     log "Is first scenario - reinstalling apps"
-    
-    uninstall_apps
-    install_app(ENV["TEST_APP_PATH"])
-    install_app(ENV["APP_PATH"])
+
     FeatureNameMemory.feature_name = feature_name
-	end
+  end
 end
 
 FeatureNameMemory = Class.new
