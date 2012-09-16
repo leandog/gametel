455b4d8edaf263f8577cf2fe614aa48be00cc604
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-09-16 05:52:40 -0400

    feature setup for brazenhead

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,31 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
 
 require 'rspec/expectations'
-require 'calabash-android/cucumber'
+require 'brazenhead'
+require 'brazenhead/server'
 require 'gametel'
 require 'gametel/navigation'
 
 World(Gametel::Navigation)
+
+keystore = {
+  :path => '~/.android/debug.keystore',
+  :alias => 'androiddebugkey',
+  :password => 'android',
+  :keystore_password => 'android'
+}
+
+server = Brazenhead::Server.new('ApiDemos/bin/ApiDemos.apk', keystore)
+
+class Driver
+  include Brazenhead
+end
+
+Before do
+  @driver = Driver.new
+  server.start("ApiDemos")
+end
+
+After do
+  server.stop
+end
