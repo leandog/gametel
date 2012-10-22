2043b41aa98fc3d2b2c7596ccd12e27e5d459de0
Levi Wilson <levi@leviwilson.com>
2012-10-21 23:38:49 -0400

    revert back to using text locators for list items in features

--- lib/gametel/platforms/brazenhead/list_item.rb
+++ lib/gametel/platforms/brazenhead/list_item.rb
@@ -5,9 +5,8 @@ module Gametel
     module ListItem
 
       def press_list_item(locator)
-        puts locator
         list = locator[:list] ? locator[:list] : 0
-        result = click_long_in_list(locator[:index] + 1, list) if locator[:index]
+        result = click_in_list(locator[:index] + 1, list) if locator[:index]
         result = click_on_text(locator[:text]) if locator[:text]
       end
     end
