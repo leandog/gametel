1923152e34558cddea787d16e9fbf57531ac12a0
Levi Wilson <levi@leviwilson.com>
2013-04-01 14:06:11 -0400

    spec for Gamtel::Waiter specifying what it is waiting for

--- spec/lib/gametel/waiter_spec.rb
+++ spec/lib/gametel/waiter_spec.rb
@@ -18,4 +18,8 @@ describe Gametel::Waiter do
   it "is not as forgiving if you make it wait" do
     lambda { wait_until(0) }.should raise_error
   end
+
+  it "can be told what it is waiting on" do
+    lambda { wait_until(0, "something to happen") }.should raise_error "Timed out waiting for something to happen"
+  end
 end
