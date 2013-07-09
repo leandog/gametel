83a0a9378097225cf25d7fcc15c4a0023f24cc84
Levi Wilson <levi@leviwilson.com>
2013-07-09 00:21:41 -0400

    remove unused methods for button properties

--- lib/gametel/platforms/brazenhead/button.rb
+++ lib/gametel/platforms/brazenhead/button.rb
@@ -16,27 +16,6 @@ module Gametel
           block.call device if block
         end
       end
-
-      def get_button_property_by_index(index, property)
-        get_button_by_index(index) do |device|
-          device.send "is_#{property}"
-        end
-        last_response.body
-      end
-
-      def get_button_property_by_id(id, property)
-        get_view_by_id(id) do |device|
-          device.send "is_#{property}"
-        end
-        last_response.body
-      end
-
-      def get_button_property_by_text(text, property)
-        get_button_by_text(text) do |device|
-          device.send "is_#{property}"
-        end
-        last_response.body
-      end
     end
   end
 end
